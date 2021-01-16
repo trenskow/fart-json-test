4ad8bdc7a8189f63090277de10536fc7940fe88f
Kristian Trenskow <trenskow@me.com>
2021-01-16 04:25:44 +0100

    Initial Commit

---
+++ fart-json-test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fart-json-test
+//
+//  Created by Kristian Trenskow on 16/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
