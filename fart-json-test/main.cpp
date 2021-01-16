cd29834e596a6b84a043b90312a3107a59bfbe2b
Kristian Trenskow <trenskow@me.com>
2021-01-16 12:30:09 +0100

    First initial passing version.

--- fart-json-test/main.cpp
+++ fart-json-test/main.cpp
@@ -5,10 +5,112 @@
 //  Created by Kristian Trenskow on 16/01/2021.
 //
 
-#include <iostream>
+#include "fart/fart.hpp"
+
+using namespace fart::io::fs;
+using namespace fart::system;
+using namespace fart::serialization;
+
+size_t skip = 0;
+size_t ran = 0;
+
+Strong<Array<String>> fullPaths(const String& path, const Array<String>& filenames) {
+	return filenames.map<String>([&path](const String& filename, const size_t idx) {
+		return path.mapCString<Strong<String>>([&filename](const char* path) {
+			return filename.mapCString<Strong<String>>([path](const char* filename) {
+				return String::format("%s%s", path, filename);
+			});
+		});
+	});
+}
+
+void fail(bool bail = true) {
+	printf("Failed\n");
+	if (bail) exit(1);
+}
+
+void pass() {
+	printf("Passed\n");
+}
+
+enum class Expectation {
+	pass = 0,
+	fail,
+	indifferent
+};
+
+void decide(Expectation expectation, const Exception* result) {
+	switch (expectation) {
+		case Expectation::pass:
+			if (result == nullptr) pass(); else fail();
+			break;
+		case Expectation::fail:
+			if (result != nullptr) pass(); else fail();
+			break;
+		case Expectation::indifferent:
+			if (result == nullptr) pass(); else fail(false);
+			break;
+	}
+}
+
+void test(const String& path, const Array<String>& filenames, Expectation expectation) {
+	filenames.forEach([&path,&expectation](const String& filename) {
+
+		ran++;
+
+		filename.substring(path.length())->withCString([](const char* filename) {
+			printf("%zu) Testing %s... ", ran, filename);
+		});
+
+		if (ran <= skip) {
+			printf("Skipped\n");
+			return;
+		}
+
+		try {
+			JSON::parse(String(File(filename, File::Mode::asRead).readToEnd()));
+		} catch (DecoderException exception) {
+			decide(expectation, &exception);
+			return;
+		} catch (JSONMalformedException exception) {
+			decide(expectation, &exception);
+			return;
+		}
+
+		decide(expectation, nullptr);
+
+	});
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+
+	if (argc == 1 || String(argv[1]) == "--help") {
+		printf("Usage: fart-json-test [test-suite-directory]\n");
+		exit(1);
+	}
+
+	String path = argv[1];
+	if (path.length() > 0 && path[path.length() - 1] != '/') path.append("/");
+
+	auto filenames = File::directoryContent(path);
+
+	auto shouldPass = fullPaths(path, filenames->filter([](const String& filename, const size_t idx) {
+		return filename[0] == 'y';
+	}));
+
+	auto shouldFail = fullPaths(path, filenames->filter([](const String& filename, const size_t idx) {
+		return filename[0] == 'n';
+	}));
+
+	auto shouldBeIndifferent = fullPaths(path, filenames->filter([](const String& filename, const size_t idx) {
+		return filename[0] == 'i';
+	}));
+
+	test(path, shouldPass, Expectation::pass);
+	test(path, shouldFail, Expectation::fail);
+	test(path, shouldBeIndifferent, Expectation::indifferent);
+
+	printf("All done.\n");
+
 	return 0;
 }
